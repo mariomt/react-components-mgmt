0e27d26b9403a8eca7934ee019af949c60f02200
mariomt <mariotinoco85@gmail.com>
2025-04-04 22:29:39 -0700

    [ADD]->Se degrado a tailwind 3.3

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import dts from 'vite-plugin-dts'
@@ -11,7 +10,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
     dts({ include: ["lib"]}),
   ],
   build: {
